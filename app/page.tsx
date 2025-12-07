01a682b521cd2533f639325a55f62b2564836181
Javasrabni <javasrabnii@gmail.com>
2025-12-07 14:22:52 +0700

    UP 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import SplashOnboarding from "@/components/splashOrOnboardingScreen/splashOnboarding";
 import ButtonVibrate from "@/components/vibrateBtn/ButtonVibrate";
 import Link from "next/link";
 
 export default function Home() {
+
  
   return (
-    <div className="m-auto max-w-[32rem] w-full bg-blue-400">
+    <div className="p-8 relative">
+      <SplashOnboarding />
       <p>tes</p>
       <Link href="/auth/register">LOGIN</Link>
       <ButtonVibrate/>
---
+++ components/splashOrOnboardingScreen/splashOnboarding.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+const SplashOnboarding = () => {
+    const [onboarding, setOnBoarding] = useState(true)
+  return (
+    <div className="absolute flex flex-col items-center justify-between p-8 inset-0 top-0 left-0 h-screen w-full bg-white">
+      <p>INI ONBOARDING</p>
+      <Image 
+        src={'/Assets/OnBoarding/boarding1.png'}
+        width={240}
+        height={240}
+        className="object-cover animate-pulse"
+        alt="Boarding"
+      />
+      <button className="w-full h-13 bg-[var(--accentColor)] text-white font-bold rounded-full cursor-pointer">Selanjutnya</button>
+    </div>
+  )
+}
+
+export default SplashOnboarding
